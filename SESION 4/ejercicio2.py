88464516dfd44ee094ef7a1961c203079b13b12a
mattiasrortega08 <mattiasrmo@gmail.com>
2025-04-08 10:26:11 -0600

    hola

---
+++ SESION 4/ejercicio2.py
@@ -0,0 +1,7 @@
+cantidad = 10
+precio = 130
+total = cantidad * precio
+
+print(f"Cantidad: {cantidad:>6}")
+print(f"Precio: {precio:>6}")
+print(f"Total: {total:>6}")
---
+++ SESION 4/ejercicio3.py
@@ -0,0 +1,11 @@
+numero1 = 25
+numero2 = 10
+
+print(f"{numero1} > {numero2} = {numero1 > numero2}")
+print(f"{numero1} < {numero2} = {numero1 < numero2}")
+print(f"{numero1} >= {numero2} = {numero1 >= numero2}")
+print(f"{numero1} <= {numero2} = {numero1 <= numero2}")
+print(f"{numero1} > {numero2} = {not(numero1 > numero2)}")
+print(f"{numero1} < {numero2} = {not(numero1 < numero2)}")
+print(f"{numero1} >= {numero2} = {not(numero1 >= numero2)}")
+print(f"{numero1} <= {numero2} = {not(numero1 <= numero2)}")
